c86f8d9fcbdece8ea74784f55f8587b8704b0f7c
Devutaraka <tarakadevu1@gmail.com>
2026-04-21 23:17:53 +0530

    UC19: Binary search for bogie ID

--- src/TrainConsistApp.java
+++ src/TrainConsistApp.java
@@ -5,22 +5,35 @@ public class TrainConsistApp {
 
     public static void main(String[] args) {
 
-        System.out.println("Train Consist Management System - UC18");
+        System.out.println("Train Consist Management System - UC19");
 
-        // Bogie IDs (unsorted)
+        // Sorted Bogie IDs (IMPORTANT)
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
-        String searchKey = "BG309"; // Change this to test
+        String searchKey = "BG309"; // Change to test
+
+        int low = 0;
+        int high = bogieIds.length - 1;
 
         boolean found = false;
 
-        // Linear Search
-        for (int i = 0; i < bogieIds.length; i++) {
+        // Binary Search
+        while (low <= high) {
 
-            if (bogieIds[i].equals(searchKey)) {
+            int mid = (low + high) / 2;
+
+            int result = searchKey.compareTo(bogieIds[mid]);
+
+            if (result == 0) {
+                System.out.println("\nBogie found at position: " + mid);
                 found = true;
-                System.out.println("\nBogie found at position: " + i);
-                break; // stop immediately when found
+                break;
+            }
+            else if (result < 0) {
+                high = mid - 1; // search left
+            }
+            else {
+                low = mid + 1; // search right
             }
         }
 
